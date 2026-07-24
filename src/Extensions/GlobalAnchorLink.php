44305cc4a47e32ba3b1b281be19797d68692bd51
Florian Thoma <florian.thoma@innoweb.com.au>
2026-07-24 16:07:36 +1000

    fix only load systemlinks and globalanchors if modules are installed

--- src/Extensions/GlobalAnchorLink.php
+++ src/Extensions/GlobalAnchorLink.php
@@ -8,70 +8,73 @@ use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Forms\DropdownField;
 
-class GlobalAnchorLink extends SuperLinkTypeExtension
-{
-    private static $extension_link_type = 'globalanchor';
+if (class_exists("Fromholdio\GlobalAnchors\GlobalAnchors")) {
 
-    private static $types = [
-        'globalanchor' => [
-            'label' => 'Global anchor',
-            'settings' => [
-                'open_in_new' => false,
-                'no_follow' => false
+    class GlobalAnchorLink extends SuperLinkTypeExtension
+    {
+        private static $extension_link_type = 'globalanchor';
+
+        private static $types = [
+            'globalanchor' => [
+                'label' => 'Global anchor',
+                'settings' => [
+                    'open_in_new' => false,
+                    'no_follow' => false
+                ]
             ]
-        ]
-    ];
+        ];
 
-    private static $db = [
-        'GlobalAnchorKey' => 'Varchar(30)'
-    ];
+        private static $db = [
+            'GlobalAnchorKey' => 'Varchar(30)'
+        ];
 
-    public function getLinkedGlobalAnchor(): ?string
-    {
-        if (!$this->isLinkTypeMatch()) return null;
-        $anchors = GlobalAnchors::get_anchors();
-        $key = $this->getOwner()->getField('GlobalAnchorKey');
-        return isset($anchors[$key]) ? $key : null;
-    }
-
-    public function updateDefaultTitle(?string &$title): void
-    {
-        if (!$this->isLinkTypeMatch()) return;
-        $anchor = $this->getOwner()->getLinkedGlobalAnchor();
-        $title = GlobalAnchors::get_anchor_title($anchor);
-    }
-
-    public function updateURL(?string &$url): void
-    {
-        if (!$this->isLinkTypeMatch()) return;
-        $anchor = $this->getOwner()->getLinkedGlobalAnchor();
-        $url = empty($anchor) ? null : '#' . $anchor;
-    }
-
-    public function updateAbsoluteURL(?string &$url): void
-    {
-        if (!$this->isLinkTypeMatch()) return;
-        $anchor = $this->getOwner()->getLinkedGlobalAnchor();
-        if (empty($anchor)) {
-            $url = null;
-            return;
+        public function getLinkedGlobalAnchor(): ?string
+        {
+            if (!$this->isLinkTypeMatch()) return null;
+            $anchors = GlobalAnchors::get_anchors();
+            $key = $this->getOwner()->getField('GlobalAnchorKey');
+            return isset($anchors[$key]) ? $key : null;
         }
-        $curr = Controller::curr();
-        $link = $curr instanceof ContentController
-            ? $curr->Link()
-            : Director::absoluteBaseURL();
-        $url = Controller::join_links($link, '#' . $anchor);
-    }
 
-    public function updateCMSLinkTypeFields($fields, $type, $fieldPrefix): void
-    {
-        if ($type !== 'globalanchor') return;
-        $fields->push(
-            DropdownField::create(
-                $fieldPrefix . 'GlobalAnchorKey',
-                _t(__CLASS__ . '.GlobalAnchor', 'Global anchor'),
-                GlobalAnchors::get_anchors()
-            )
-        );
+        public function updateDefaultTitle(?string &$title): void
+        {
+            if (!$this->isLinkTypeMatch()) return;
+            $anchor = $this->getOwner()->getLinkedGlobalAnchor();
+            $title = GlobalAnchors::get_anchor_title($anchor);
+        }
+
+        public function updateURL(?string &$url): void
+        {
+            if (!$this->isLinkTypeMatch()) return;
+            $anchor = $this->getOwner()->getLinkedGlobalAnchor();
+            $url = empty($anchor) ? null : '#' . $anchor;
+        }
+
+        public function updateAbsoluteURL(?string &$url): void
+        {
+            if (!$this->isLinkTypeMatch()) return;
+            $anchor = $this->getOwner()->getLinkedGlobalAnchor();
+            if (empty($anchor)) {
+                $url = null;
+                return;
+            }
+            $curr = Controller::curr();
+            $link = $curr instanceof ContentController
+                ? $curr->Link()
+                : Director::absoluteBaseURL();
+            $url = Controller::join_links($link, '#' . $anchor);
+        }
+
+        public function updateCMSLinkTypeFields($fields, $type, $fieldPrefix): void
+        {
+            if ($type !== 'globalanchor') return;
+            $fields->push(
+                DropdownField::create(
+                    $fieldPrefix . 'GlobalAnchorKey',
+                    _t(__CLASS__ . '.GlobalAnchor', 'Global anchor'),
+                    GlobalAnchors::get_anchors()
+                )
+            );
+        }
     }
 }
--- src/Extensions/SystemLink.php
+++ src/Extensions/SystemLink.php
@@ -7,50 +7,54 @@ use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\View\ArrayData;
 
-class SystemLink extends SuperLinkTypeExtension
-{
-    private static $extension_link_type = 'system';
+if (class_exists("Fromholdio\SystemLinks\SystemLinks")) {
 
-    private static $types = [
-        'system' => [
-            'label' => 'System link',
-            'settings' => [
-                'no_follow' => false
+    class SystemLink extends SuperLinkTypeExtension
+    {
+        private static $extension_link_type = 'system';
+
+        private static $types = [
+            'system' => [
+                'label' => 'System link',
+                'settings' => [
+                    'no_follow' => false
+                ]
             ]
-        ]
-    ];
+        ];
 
-    private static $db = [
-        'SystemLinkKey' => 'Varchar(30)'
-    ];
+        private static $db = [
+            'SystemLinkKey' => 'Varchar(30)'
+        ];
 
-    public function getLinkedSystemLink(): ?ArrayData
-    {
-        if (!$this->isLinkTypeMatch()) return null;
-        return SystemLinks::get_link($this->getOwner()->getField('SystemLinkKey'));
+        public function getLinkedSystemLink(): ?ArrayData
+        {
+            if (!$this->isLinkTypeMatch()) return null;
+            return SystemLinks::get_link($this->getOwner()->getField('SystemLinkKey'));
+        }
+
+        public function updateDefaultTitle(?string &$title): void
+        {
+            if (!$this->isLinkTypeMatch()) return;
+            $title = $this->getOwner()->getLinkedSystemLink()?->getField('Title');
+        }
+
+        public function updateURL(?string &$url): void
+        {
+            if (!$this->isLinkTypeMatch()) return;
+            $url = $this->getOwner()->getLinkedSystemLink()?->getField('URL');
+        }
+
+        public function updateCMSLinkTypeFields(FieldList $fields, string $type, string $fieldPrefix): void
+        {
+            if (!$this->isLinkTypeMatch($type)) return;
+            $fields->push(
+                DropdownField::create(
+                    $fieldPrefix . 'SystemLinkKey',
+                    _t(__CLASS__ . '.SystemLink', 'System Link'),
+                    SystemLinks::get_map()
+                )
+            );
+        }
     }
 
-    public function updateDefaultTitle(?string &$title): void
-    {
-        if (!$this->isLinkTypeMatch()) return;
-        $title = $this->getOwner()->getLinkedSystemLink()?->getField('Title');
-    }
-
-    public function updateURL(?string &$url): void
-    {
-        if (!$this->isLinkTypeMatch()) return;
-        $url = $this->getOwner()->getLinkedSystemLink()?->getField('URL');
-    }
-
-    public function updateCMSLinkTypeFields(FieldList $fields, string $type, string $fieldPrefix): void
-    {
-        if (!$this->isLinkTypeMatch($type)) return;
-        $fields->push(
-            DropdownField::create(
-                $fieldPrefix . 'SystemLinkKey',
-                _t(__CLASS__ . '.SystemLink', 'System Link'),
-                SystemLinks::get_map()
-            )
-        );
-    }
 }
